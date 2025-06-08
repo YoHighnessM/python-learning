0d7e0862dde3a650c9f16a480489f0b81f7d9024
Yohannes Mulu <yohannesmulu.gm.dev@gmail.com>
2025-06-08 13:54:53 +0300

    day two code

---
+++ 100-days-angela/day02/main.py
@@ -0,0 +1,12 @@
+# # print(type("hello"))
+# # print(type(10))
+# # print(type(10.4))
+# # print(type(True))
+
+
+# # print("Number of letters in your name: + len(input("Enter your name"))")
+
+# name = input("Enter your name: ")
+# nameLen = len(name)
+
+# print("Number of letters in your name: ", str(nameLen))
