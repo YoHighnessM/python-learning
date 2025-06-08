60e9e6a288face4df75fed95d0d7af515a30c835
Yohannes Mulu <yohannesmulu.gm.dev@gmail.com>
2025-06-08 12:12:52 +0300

    simple band name generator using inputs

---
+++ 100-days-angela/day01/challenge.py
@@ -0,0 +1,8 @@
+print("Welcome to the Brad Name Generator")
+
+cityName = input("What's the name of the city you grew up in? ")
+petName = input("What's your pet name? ")
+
+finalResult = f"Your band name could be {cityName} {petName}"
+
+print(finalResult)
