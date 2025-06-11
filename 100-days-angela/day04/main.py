42ce333a31dc63f72e7fd159a2274f651118d4cc
Yohannes Mulu <yohannesmulu.gm.dev@gmail.com>
2025-06-11 23:40:01 +0300

    day four code

---
+++ 100-days-angela/day04/main.py
@@ -0,0 +1,27 @@
+import random
+
+# randInt = random.randint(1, 10)
+# print(randInt)
+
+#
+
+# randomFlotat = random.random() * 10
+# print(randomFlotat)
+
+#
+
+# randomNum = random.randint(1, 2)
+
+# if randomNum == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+#
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# randomFriend = random.randint(0, 4)
+# print(friends[randomFriend])
+
+print(random.choice(friends))
