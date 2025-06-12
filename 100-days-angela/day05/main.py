e06e47b1413d35e42bb555822f8d7aae12f594ba
Yohannes Mulu <yohannesmulu.gm.dev@gmail.com>
2025-06-12 23:28:57 +0300

    day five code

---
+++ 100-days-angela/day05/main.py
@@ -0,0 +1,21 @@
+# scores = [1, 2, 3, 4, 5]
+
+# sum = 0
+# for score in scores:
+#     sum += score
+# print(sum)
+
+#
+
+# lists = [1, 2, 3, 4, 5, 99]
+
+# max = 0
+# for list in lists:
+#     if list > max:
+#         max = list
+# print(max)
+
+sum = 0
+for number in range(1, 101):
+    sum += number
+print(sum)
