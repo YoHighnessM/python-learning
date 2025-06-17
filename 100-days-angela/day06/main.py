b5ce3819bb80476cb3646818885f940ac6bad7ff
Yohannes Mulu <yohannesmulu.gm.dev@gmail.com>
2025-06-18 00:09:07 +0300

    day six code

---
+++ 100-days-angela/day06/main.py
@@ -0,0 +1,57 @@
+# def my_function():
+#     print("hello")
+
+
+# my_function()
+
+for num in range(0, 6):
+    print("hello")
+
+
+#
+#
+#
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# def one_cycle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+
+# while not at_goal():
+#     while front_is_clear() and wall_on_right() == True:
+#         move()
+
+#     if wall_in_front() and right_is_clear() == False:
+#         turn_left()
+#     elif wall_in_front() and right_is_clear():
+#         turn_right()
+#         move()
+#         turn_right()
+#     elif wall_in_front():
+#         turn_left()
+#     elif wall_in_front() and wall_on_right() == True:
+#         move()
+#     elif wall_in_front() and wall_on_right() == False:
+#         turn_right()
+#         move()
+#         turn_right()
+
+#     if wall_on_right():
+#         move()
+#     elif wall_on_right() == False:
+#         turn_right()
+#         move()
+#         turn_right()
+#         move()
