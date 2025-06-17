b81b6897a4cf66b77821a8cfb67e8125be06dcb8
Yohannes Mulu <yohannesmulu.gm.dev@gmail.com>
2025-06-18 00:10:45 +0300

    reeborg world maze challenge

---
+++ practice-challenges/reeborg's world/maze.py
@@ -0,0 +1,17 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+
+
+## after day 15 come back to this code and do it again
