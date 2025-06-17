397c623f0b47402f0cabd98f09053c0033a3ad66
Yohannes Mulu <yohannesmulu.gm.dev@gmail.com>
2025-06-18 00:10:02 +0300

    reeborg world around 2 challenge

---
+++ practice-challenges/reeborg's world/around2.py
@@ -0,0 +1,24 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# def move_forward():
+#     while front_is_clear():
+#         move()
+#         while right_is_clear():
+#             turn_right()
+#             move()
+
+#     turn_left()
+#     move()
+#     move()
+#     move()
+
+
+# move_forward()
+# move_forward()
+# move_forward()
+# move_forward()
+# move_forward()
