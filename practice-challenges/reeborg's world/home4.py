31d8b26281bd802c11e750c0fe99dd596d47299c
Yohannes Mulu <yohannesmulu.gm.dev@gmail.com>
2025-06-18 00:10:29 +0300

    reeborg world home 4 challenge

---
+++ practice-challenges/reeborg's world/home4.py
@@ -0,0 +1,27 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def repeatThree():
+#     repeat 3:
+#         move()
+
+# def firstHalf():
+#     repeatThree()
+#     turn_left()
+#     repeatThree()
+
+# firstHalf()
+# turn_right()
+# move()
+# turn_right()
+# firstHalf()
+# turn_right()
+# move()
+# turn_right()
+# firstHalf()
+# turn_right()
+# move()
+# turn_right()
+# firstHalf()
