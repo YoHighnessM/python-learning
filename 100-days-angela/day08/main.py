8f7fb407302bed61912d01be50a168e8aa53cd95
Yohannes Mulu <yohannesmulu.gm.dev@gmail.com>
2025-06-20 00:45:31 +0300

    day eight code

---
+++ 100-days-angela/day08/main.py
@@ -0,0 +1,17 @@
+# name = input("What is your name? ")
+
+
+# def greet(user_name):
+#     print("Hello")
+#     print(f"Welcome {user_name}")
+
+
+# greet(name)
+
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+
+greet_with(location="Bole", name="Jo")
